23a5835bfdc8e400b68309905958fd098ecc5245
Wynn Blevins <wynnblevins@yahoo.com>
2018-06-11 11:05:43 -0400

    Created basic guitar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GuitarComponent from './components/guitarComponent';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <GuitarComponent></GuitarComponent>
       </div>
     );
   }
---
+++ src/components/guitarComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class GuitarComponent extends Component {
+  render() {
+    return (<h1>This is my GuitarComponent</h1>);
+  }
+}
+
+export default GuitarComponent;

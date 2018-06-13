376bd740e1d71e5ef8aaa554d32535bbde54bc40
Wynn Blevins <wynnblevins@yahoo.com>
2018-06-13 11:32:36 -0400

    Guitars now appear in a four by four grid on the main page.  Gameboard only displays the name of the guitar, aka no images yet.

--- src/components/guitarGameBoard.js
+++ src/components/guitarGameBoard.js
@@ -1,23 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import GuitarComponent from './guitarComponent';
 
-class GuitarGameBoard extends Component {
-  render() {
-    const { guitars } = this.props;
-    const guitarsList = guitars.map(guitar => {
-      console.log(guitar.name);
-    })
-    
-    return (
-      <div>
-      { 
-        this.props.guitars.map(function(guitar) {
-          return <GuitarComponent name={guitar.name}></GuitarComponent>
-        })
-      }
-      </div>
-    );
-  }
+const styles = theme => ({
+  container: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(12, 1fr)',
+    gridGap: `${theme.spacing.unit * 3}px`,
+  },
+  paper: {
+    padding: theme.spacing.unit,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    whiteSpace: 'nowrap',
+    marginBottom: theme.spacing.unit,
+  },
+  divider: {
+    margin: `${theme.spacing.unit * 2}px 0`,
+  },
+});
+
+function GuitarGameBoard(props) {
+  const { guitars } = props;
+
+  return (
+    <div>
+      <Typography variant="subheading" gutterBottom>
+        
+      </Typography>
+      <Grid container spacing={16}>
+        {guitars.map((guitar) => (
+          <Grid item xs={3}>
+            <GuitarComponent name={guitar.name}></GuitarComponent>
+          </Grid>
+        ))}
+        
+      </Grid>
+      <Typography variant="subheading" gutterBottom>
+        CSS Grid Layout:
+      </Typography>
+    </div>
+  );
 }
 
-export default GuitarGameBoard;
+GuitarGameBoard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(GuitarGameBoard);
